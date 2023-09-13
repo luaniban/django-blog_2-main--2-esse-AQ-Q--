a68a1ddeaa3d43e2f782152a011614775310e696
Luan <luanibanhez@gmail.com>
2023-09-13 10:18:58 -0300

    atualização

--- django-blog_2-main/blog/urls.py
+++ django-blog_2-main/blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from blog.views import index, ola, post_show, PostDetailView # Nesta linha importamos as views
+from blog.views import index, ola, post_show, PostDetailView, get_all_posts, get_post
 
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('ola/', ola, name='ola' ),
-    path('posts/all', ola, name="posts_list"), # a view responsável é ola()
+    path('posts/all', ola, name="posts_list"),
     path('post/<int:post_id>', post_show, name="exibe_post"),
     path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+    path('api/posts', get_all_posts, name="posts_data"),
+    path('api/posts/<int:post_id>', get_post, name="post_data"),
 
 ]
